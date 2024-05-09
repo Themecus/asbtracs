fc046d2712ae3f62b34327aa861861492b9fe982
Themecus <miguelangelbros2015@gmail.com>
2024-05-08 21:19:54 -0400

    aqui1

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+      Pera a= new Pera("composta","b");
+      a.metodo1();
+      a.metodo2();
+      a.sabor();
+      a.textura();
+      a.setNombre("composta");
+
+
+    }
+}
